570f56b3dda01660163536c5e6d7ca8c4e76a03a
aenbeel <48176215+aenbeel@users.noreply.github.com>
2020-02-29 19:10:48 +0100

    Implement first draft of scanner
    It currently recognizes:
    + single and multi special chars (parens, equal, bang-equal etc...)
    + identifiers
    + some keywords: if, else, return

--- src/julox/scanner.clj
+++ src/julox/scanner.clj
@@ -1,83 +1,102 @@
-(ns julox.scanner)
+(ns julox.scanner
+    (:require [clojure.set :refer [union]])
+    (:require [clojure.java.io :as io]))
+
+(def special-words #{:if :else :return})
+
+(def special-sequences
+  {\( {:type "left-paren"
+       :next {}}
+
+   \) {:type "right-paren"
+       :next  {}}
+
+   \{ {:type "left-brace"
+       :next  {}}
+
+   \} {:type "right-brace"
+       :next  {}}
+
+   \, {:type "comma"
+       :next  {}}
+
+   \. {:type "dot"
+       :next  {}}
+
+   \- {:type "minus"
+       :next  {}}
+
+   \+ {:type "plus"
+       :next  {}}
+
+   \; {:type "semicolon"
+       :next  {}}
+
+   \* {:type "star"
+       :next  {}}
+
+   \! {:type "bang" 
+       :next  {\= {:type "-equal"}}}
+
+   \= {:type "equal" 
+       :next  {\= {:type "-equal"}}}
+
+   \> {:type "greater" 
+       :next  {\= {:type "-equal"}}}
+
+   \< {:type "less" 
+       :next  {\= {:type "-equal"}}}})
 
 (defn init-lex [source]
   {:line 1
    :source source
    :tokens []})
 
-(def singles
-  {\( :left-paren
-   \) :right-paren
-   \{ :left-brace
-   \} :right-brace
-   \, :comma
-   \. :dot
-   \- :minus
-   \+ :plus
-   \; :semicolon
-   \* :star })
-
-(def maybe-doubles
-  {\! [\= :bang-equal :bang]
-   \= [\= :equal-equal :equal]
-   \< [\= :less-equal :less]
-   \> [\= :greater-equal :greater]})
-
-(def multi-char-tokens
-  {\( ["left-paren"]
-   \) ["right-paren"]
-   \{ ["left-brace"]
-   \} ["right-brace"]
-   \, ["comma"]
-   \. ["dot"]
-   \- ["minus"]
-   \+ ["plus"]
-   \; ["semicolon"]
-   \* ["star"]
-   \! ["bang" \= "-equal"]
-   \= ["equal" \= "-equal"]
-   \< ["less" \= "-equal"]})
-
-\a ["bee" [\= "-too"]]
-(multi-char-tokens ch)
-
-(str (str \a) \b)
-
-
-
-(defn concat-chars [ch source type-segments]
-  (loop [chs source
-         segments type-segments
-         typ (str (first path))]
-    (let [segment (first segments)])
-    (if (= source-ch path-ch)
-      (recur (rest chs) (rest path) (str identifier fragment))
-      (key identifier))))
-oo
-(loop [source source
-       path (conj ) match
-       token-type ""]
-  (let [ch (first source)
-        frag (first path)]
-    (if (= ch frag))))
-
-
-{
- \! ["bang" \= ["-equal"]]}
-
-(defn identifier [source & sets]
-  (loop [source source
-         sets sets
-         value ""]
-    (let [ch (first source)
-          valid-set (first sets)]
-    (if (and source (valid-set ch))
-      (recur (rest source) (rest sets) (conj value ch))
-      (token *missing* value)))))
-
 (defn token [typ value]
   {:type typ
    :value value})
+
+(defn error-msg [line message]
+  (str "[line:" line "] LexerError: " message))
+
+(defn char-set
+  "Generate set containing all characters in the range [from; to]"
+  [from to]
+  (set (map char (range (int from) (inc (int to))))))
+
+(def identifier-start (union (char-set \A \Z)
+                             (char-set \a \z)
+                             #{\_}))
+(def identifier-rest (union identifier-start
+                            (char-set \0 \9)))
+
+(defn find-identifier [source sets]
+  (let [initial (:first sets)
+        subsequent (:rest sets)]
+  ;; (assert (initial (first source))
+  ;;         (error-msg "Identifier cannot start with a number"))
+  (loop [src source
+         value ""]
+    (if (subsequent (first src))
+      (recur (rest src) (str value (first src)))
+      (let [normal? (not (special-words (keyword value)))]
+        {:token  (token (if normal? :identifier :keyword) value)
+         :count (count value)})))))
+
+
+(defn find-specials [source ch info]
+  (loop [src source
+         segment (array-map ch info)
+         typ ""
+         value ""]
+    (let [entry (first segment)]
+      (if (and entry (= (first src) (key entry)))
+        (recur (rest src)
+               (:next (val entry))
+               (str typ (:type (val entry)))
+               (str value (key entry)))
+        {:token (token (keyword typ) value)
+         :count (count value)}))))
 
 (defn add-token [lex token]
   (let [line (:line lex)]
@@ -86,34 +105,53 @@ oo
             conj
             (assoc token :line line))))
 
+
 (defn advance
   ([lex]
    (assoc lex :source (rest (:source lex))))
   ([lex n]
-   (nth (iterate advance lex) (inc n))))
+   (nth (iterate advance lex) n)))
+
 
 (defn inc-line [lex]
   (update lex :line inc))
 
+
 (defn next-token [lex]
-    (let [ch (first (:source lex))
-          chs (rest (:source lex))]
-      (cond
-        (or (= \space ch) 
-            (= \tab ch)) (advance lex)
+  (let [src (:source lex)
+        ch (first src)]
+    (cond
+      (or (= \space ch) 
+          (= \tab ch)) (advance lex)
 
-        (= \newline ch) (inc-line (advance lex))
+      (= \newline ch) (inc-line (advance lex))
 
-        (singles ch) (add-token lex (token (singles ch) ch))
+      (multi-char-token? ch)
+      (let [finder (if (special-sequences ch)
+                     (fn [source]
+                       (find-specials source ch (special-sequences ch)))
+                     (fn [source]
+                       (find-identifier source {:first identifier-start
+                                                :rest identifier-rest})))
+            result (finder src)]
+        (-> lex
+            (add-token (:token result))
+            (advance (:count result))))
 
-        (and (maybe-doubles ch)) (select-double lex ch (rest (:source lex))) 
+      :else (do (println "Unexpected character.")
+                lex))))
 
-        :else (println "Unexpected character."))))
-
+(defn multi-char-token? [ch]
+  (or (special-sequences ch)
+      (identifier-start ch)))
 
 
 (defn tokenize [source]
-  (loop [progress (init-progress)
-         tokens []]
-    (if-let [token (next-token source progress)]
-      (recur progress (conj tokesn token)))))
+  (loop [lex (init-lex source)]
+    (if (empty? (:source lex))
+      (:tokens lex)
+      (recur (next-token lex)))))
+
+
+
+(clojure.pprint/pprint (tokenize (slurp (io/resource "lox/main.lx"))))
