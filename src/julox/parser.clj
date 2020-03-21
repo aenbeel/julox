3b88f35ff53c01583c950a037f9fca56c398e13e
aenbeel <48176215+aenbeel@users.noreply.github.com>
2020-03-21 14:01:59 +0100

    Start prototyping a parser implementation

--- src/julox/parser.clj
+++ src/julox/parser.clj
@@ -17,3 +17,22 @@
 
 :number tools/rand-num}
 
+{:expr            [:equality]
+ :equality        [:comparison (repeat [#{"!=" "=="} :comparison])]
+ :comparison      [:addition (repeat [#{">" ">=" "<" "<="} :addition])]
+ :addition        [:multiplication (repeat [#{"+" "-"} :multiplication])]
+ :multiplication  [:unary (repeat [#{"/" "*"} :unary])]
+ :unary           #{[#{"!" "-"} :unary] :primary}
+ :primary         #{:number :string "false" "true" "nil" ["(" :expr ")"]}}
+
+(let [token (first tokens)
+      rule (grammar rule-name)]
+  (if (or typ value)))
+
+(defn check-type [token typ]
+  ())
+(defn check-value [token value])
+
+(defn match [ast token])
+(def s #{:a :b :c})
+(* (rand) (count s)) (vec #{:a :b})
